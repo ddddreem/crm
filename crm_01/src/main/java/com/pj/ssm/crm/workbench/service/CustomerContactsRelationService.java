4a86f14d3c5e076544219c699e897550049d98a2
dreem <ddddreem@163.com>
2022-05-25 12:12:02 +0800

    master 删除客户联系人关系的service

---
+++ crm_01/src/main/java/com/pj/ssm/crm/workbench/service/CustomerContactsRelationService.java
@@ -0,0 +1,10 @@
+package com.pj.ssm.crm.workbench.service;
+
+/**
+ * @Description: 此处添加描述 //TODO
+ * @Author: Dreem
+ * @Date: 2022 05 25 12:08
+ **/
+public interface CustomerContactsRelationService {
+
+}
---
+++ crm_01/src/main/java/com/pj/ssm/crm/workbench/service/impl/CustomerContactsRelationImpl.java
@@ -0,0 +1,13 @@
+package com.pj.ssm.crm.workbench.service.impl;
+
+import com.pj.ssm.crm.workbench.service.CustomerContactsRelationService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description: 此处添加描述 //TODO
+ * @Author: Dreem
+ * @Date: 2022 05 25 12:09
+ **/
+@Service("customerContactsRelationService")
+public class CustomerContactsRelationImpl implements CustomerContactsRelationService {
+}
